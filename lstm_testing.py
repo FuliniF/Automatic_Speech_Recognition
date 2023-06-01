2e5d8d83e0196b9a144a6a55d8c4ed3342418b97
FuliniF <109399083+FuliniF@users.noreply.github.com>
2023-06-02 00:15:10 +0800

    Update lstm_testing.py

--- lstm_testing.py
+++ lstm_testing.py
@@ -1,8 +1,3 @@
-# this .py file is for testing lstm model, not for ASR.
-# predict Tesla stock price
-
-# to-do: 將一維資料輸入調整為二維資料
-
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -13,42 +8,17 @@ import matplotlib.pyplot as plt
 # from sklearn.preprocessing import MinMaxScaler
 import preprocess
 
-# def window_data(data, window_size):
-#     X = []
-#     y = []
-#     i = 0
-#     while (i+window_size < len(data)):
-#         X.append(data[i : i+window_size]) # X: train data per group
-#         y.append(data[i+window_size])     # y: target
-#         i = i + 1
-#     assert len(X) == len(y) # check whether X and y have equal length
-#     return X, y
-
 if __name__ == "__main__":
     trainX, testX, trainY, testY = preprocess.getTrainTest("mfcc")
     batchSize, windowSize = preprocess.getBatchWindow(trainX)
-
-    # data preprocessing
-    # tesla_stocks = pd.read_csv('tesla_stocks.csv')
-    # data_to_use = tesla_stocks['Close'].values
-    # scaler = MinMaxScaler(feature_range = (0, 1))
-    # scaled_data = scaler.fit_transform(data_to_use.reshape(-1, 1))
-
-    # window_size = 7
-
-    # X, y = window_data(scaled_data, window_size)
-
-    # divide some data to be testing data
-    # X_train = np.array(X[:700])
-    # y_train = np.array(y[:700])
-    # X_test = np.array(X[700:])
-    # y_test = np.array(y[700:])
+    trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+    trainY = np.reshape(trainY, (trainY.shape[0], trainY.shape[1], 1))
 
     # Initialising the RNN
     model = Sequential()
 
     # Adding the LSTM layers and some Dropout regularisation
-    model.add(LSTM(units = 50, return_sequences = True, input_shape = (trainX.shape[1], 1)))
+    model.add(LSTM(units = 50, return_sequences = True, input_shape = (trainX.shape[1:])))
     model.add(Dropout(0.2))
     model.add(LSTM(units = 50, return_sequences = True))
     model.add(Dropout(0.2))
@@ -73,14 +43,4 @@ if __name__ == "__main__":
     acc = hist.history['accuracy'][-1]
     plt.title('Loss: %.3f, Accuracy: %.3f' % (loss, acc))
     plt.savefig("./Plots/test.png")
-
-
-
-    # plt.plot(scaled_data, color = 'red', label = 'Real')  # red: real price
-    # plt.plot(predicted, color = 'blue', label = 'Predicted')  # blue: predicted price
-    # plt.title('Prediction')
-    # plt.xlabel('Time')
-    # plt.ylabel('Tesla Stock Price')
-    # plt.legend(loc="best")
-    # plt.savefig("./Plots/test.png")
-    # plt.close()
+    plt.close()
